6a98f2cd3620c761acb59e9009b89320ee5b79eb
lemonbenbenben <lhhan1@gmail.com>
2019-09-20 19:41:03 -0500

    first test upload

---
+++ Untitled9.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 2. Array questions
+
+# In[ ]:
+
+
+##1085. Sum of Digits in the Minimum Number
+class Solution:
+    def sumOfDigits(self, A):
+        m=min(A)
+        quotient=1
+        r=[]
+        while quotient>0: 
+#             quotient,remainder=divmod(m,10)
+            quotient=m//10
+            remainder=m%10
+            r.append(remainder)
+            m=quotient
+        if sum(r)%2==0:
+            return 1
+        else:
+            return 0
+
